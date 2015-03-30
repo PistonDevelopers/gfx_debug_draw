b57232fa0c86e550525c6b524d7f95fa847a0b2c
Steve Jahns <s.t.jahns@gmail.com>
2015-03-29 21:59:31 -0600

    Fixed grow_buffer.

--- src/utils.rs
+++ src/utils.rs
@@ -12,8 +12,9 @@ pub fn grow_buffer<D: Device, F: Factory<D::Resources>, T>(
     buffer: &RawBufferHandle<D::Resources>,
     required_size: usize,
 ) -> RawBufferHandle<D::Resources> {
-    let mut size = buffer.get_info().size / mem::size_of::<T>();
-    while size < required_size {
+    let required_size_bytes = required_size * mem::size_of::<T>();
+    let mut size = buffer.get_info().size;
+    while size < required_size_bytes {
         size *= 2;
     }
     factory.create_buffer_raw(size, BufferUsage::Dynamic)
