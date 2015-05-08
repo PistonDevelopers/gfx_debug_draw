e0b71313bdc27458e5dfe7c68ffcb0c6c468e4d1
Kagami Hiiragi <kagami@genshiken.org>
2015-05-08 20:19:29 +0300

    Export DebugRendererError

--- src/lib.rs
+++ src/lib.rs
@@ -14,5 +14,5 @@ mod text_renderer;
 mod bitmap_font;
 mod utils;
 
-pub use debug_renderer::DebugRenderer;
+pub use debug_renderer::{DebugRenderer, DebugRendererError};
 pub use bitmap_font::BitmapFont;
